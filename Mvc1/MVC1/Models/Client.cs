57770dca305f8ef74024980a7d0de6902b2a543c
Mayra Cabrera <mayracabrerajaldin@gmail.com>
2019-04-25 10:31:35 -0400

    3 commit con Cliente

---
+++ Mvc1/MVC1/Models/Client.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC1.Models
+{
+    public class Client
+   {
+    
+    [Key]
+    public int ClientId { get; set; }
+    [Required]
+    public string Name { get; set; }
+    public string Email { get; set; }
+
+}
+}
--- Mvc1/MVC1/Models/DataContext.cs
+++ Mvc1/MVC1/Models/DataContext.cs
@@ -8,11 +8,13 @@ namespace MVC1.Models
 {
     public class DataContext:DbContext
     {
-        public DataContext():base("DefaultConnection")
+        public DataContext():base("DefaultConnection4")
         {
 
         }
 
         public System.Data.Entity.DbSet<MVC1.Models.Product> Products { get; set; }
+
+        public System.Data.Entity.DbSet<MVC1.Models.Client> Clients { get; set; }
     }
 }
