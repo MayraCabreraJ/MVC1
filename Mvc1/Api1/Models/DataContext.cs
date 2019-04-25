3ea1567f123b3be21a83974cf256108ba79fe4bf
Mayra Cabrera <mayracabrerajaldin@gmail.com>
2019-04-25 11:08:35 -0400

    5 Commit con WEB API

---
+++ Mvc1/Api1/Models/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Api1.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnectionC")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<Api1.Models.Product> Products { get; set; }
+
+        public System.Data.Entity.DbSet<Api1.Models.Client> Clients { get; set; }
+
+        public System.Data.Entity.DbSet<Api1.Models.Sale> Sales { get; set; }
+    }
+}
---
+++ Mvc1/Api1/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Api1.Models
+{
+    public class Product
+    {
+        
+        [Required]
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
